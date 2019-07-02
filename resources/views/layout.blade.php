300e8bc52a5b9b1121aa08db1dfb90169331b6dc
martingrondein <martingrondein@gmail.com>
2019-07-02 14:58:36 +0200

    Massive improvement to UI

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,13 +4,27 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Contact List</title>
+  <title>Contactor</title>
   <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Contactor</a>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="/contact">Contacts</a></li>
+            <li class="nav-item"><a class="nav-link" href="/city">City</a></li>
+          </ul>
+        </div>
+      </nav>
   <div class="container">
     @yield('content')
   </div>
+  <nav class="navbar navbar-expand-lg fixed-bottom navbar-dark bg-dark">
+      <span class="navbar-text text-muted">Version 0.0.2</span>
+  </nav>
   <script src="{{ asset('js/app.js') }}" type="text/js"></script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,57 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        <title>Laravel</title>
+@section('content')
+<style>
+.uper {
+    margin-top: 40px;
+}
+</style>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+<div class="uper">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <div class="container">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        {{-- Row 1 --}}
+        <div class="row">
+            <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Contacts</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Contact Management</h6>
+                    <p class="card-text">
+                        Area to manage your contact list. Create, edit or delete contacts.
+                    </p>
+                    <a href="/contact" class="card-link">Manage Contacts</a>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                   Contactor
                 </div>
-
-                <div class="links">
-                    <a href="/contact">Manage Contacts</a>
-                    <a href="/city">Manage Cities</a>
-
+            </div>
+            <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Cities</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">City Management</h6>
+                    <p class="card-text">
+                        Area to manage your various citites. Create, edit or delete cities.
+                    </p>
+                    <a href="/city" class="card-link">Manage Cities</a>
+                </div>
+            </div>
+            </div>
+            <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Coming Soon</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">In Development</h6>
+                    <p class="card-text">
+                        This section is currently under development for a future release.
+                        Check back soon.
+                    </p>
+                    <a href="#" class="card-link">Coming Soon</a>
+                </div>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+
+    </div>
+</div>
+@endsection
